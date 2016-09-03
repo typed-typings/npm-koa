48501fa1bb1f4278a175a5ca572ead20db18b4d4
jKey Lu <jkeylu@gmail.com>
2016-09-03 16:35:16 +0800

    use class to declare Application

--- lib/application.d.ts
+++ lib/application.d.ts
@@ -6,69 +6,65 @@ import { BaseRequest, Request as _Request } from './request';
 import { BaseResponse, Response as _Response } from './response';
 import { BaseContext, Context as _Context } from './context';
 
-declare namespace app {
-    export type Request = _Request;
-    export type Response = _Response;
-    export type Context = _Context;
+declare class Application extends EventEmitter {
+    proxy: boolean;
+    middleware: compose.Middleware<Context>[];
+    subdomainOffset: number;
+    env: string;
+    context: BaseContext;
+    request: BaseRequest;
+    response: BaseResponse;
+    silent: boolean;
+    keys: Keygrip | string[];
 
-    export interface Application extends EventEmitter {
-        proxy: boolean;
-        middleware: compose.Middleware<Context>[];
-        subdomainOffset: number;
-        env: string;
-        context: BaseContext;
-        request: BaseRequest;
-        response: BaseResponse;
-        silent: boolean;
-        keys: Keygrip | string[];
+    constructor();
 
-        /**
-         * Shorthand for:
-         *
-         *    http.createServer(app.callback()).listen(...)
-         */
-        listen(port: number, hostname?: string, backlog?: number, callback?: Function): http.Server;
-        listen(port: number, hostname?: string, callback?: Function): http.Server;
-        listen(path: string, callback?: Function): http.Server;
-        listen(handle: any, listeningListener?: Function): http.Server;
-        listen(): http.Server;
+    /**
+     * Shorthand for:
+     *
+     *    http.createServer(app.callback()).listen(...)
+     */
+    listen(port: number, hostname?: string, backlog?: number, callback?: Function): http.Server;
+    listen(port: number, hostname?: string, callback?: Function): http.Server;
+    listen(path: string, callback?: Function): http.Server;
+    listen(handle: any, listeningListener?: Function): http.Server;
+    listen(): http.Server;
 
-        /**
-         * Return JSON representation.
-         * We only bother showing settings.
-         */
-        inspect(): any;
+    /**
+     * Return JSON representation.
+     * We only bother showing settings.
+     */
+    inspect(): any;
 
-        /**
-         * Return JSON representation.
-         * We only bother showing settings.
-         */
-        toJSON(): any;
+    /**
+     * Return JSON representation.
+     * We only bother showing settings.
+     */
+    toJSON(): any;
 
-        /**
-         * Use the given middleware `fn`.
-         *
-         * Old-style middleware will be converted.
-         */
-        use(middleware: compose.Middleware<Context>): this;
+    /**
+     * Use the given middleware `fn`.
+     *
+     * Old-style middleware will be converted.
+     */
+    use(middleware: compose.Middleware<Context>): this;
 
-        /**
-         * Return a request handler callback
-         * for node's native http server.
-         */
-        callback(): (req: http.IncomingMessage, res: http.ServerResponse) => void;
+    /**
+     * Return a request handler callback
+     * for node's native http server.
+     */
+    callback(): (req: http.IncomingMessage, res: http.ServerResponse) => void;
 
-        /**
-         * Default error handler.
-         */
-        onerror(err: Error): void;
-    }
-
-    export interface Koa {
-        new (): Application;
-    }
+    /**
+     * Default error handler.
+     */
+    onerror(err: Error): void;
 }
 
-declare const app: app.Koa;
+declare namespace Application {
+    export interface Request extends _Request {};
+    export interface Response extends _Response {};
+    export interface Context extends _Context {};
+}
 
-export = app;
+export = Application;
